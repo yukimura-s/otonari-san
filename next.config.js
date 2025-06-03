529e695bfd1a8e4197bb215236ce82f3254d74b1
yukimura-s <cafereon365@gmail.com>
2025-06-03 23:41:11 +0900

    Simplify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const fs = require('fs');
-const path = require('path');
+const nextConfig = {}
 
-const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  server: {
-    https: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.https = {
-        key: fs.readFileSync(path.join(__dirname, 'certs/localhost.key')),
-        cert: fs.readFileSync(path.join(__dirname, 'certs/localhost.crt')),
-      };
-    }
-    return config;
-  },
-};
-
-module.exports = nextConfig; 
+module.exports = nextConfig 
